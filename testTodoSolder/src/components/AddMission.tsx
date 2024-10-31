12142cee7624cdbb681ba7f0a70ed49842eeb882
Meni <menil@MENI.com>
2024-10-31 10:33:31 +0200

    update interface from Add mission

--- testTodoSolder/src/components/AddMission.tsx
+++ testTodoSolder/src/components/AddMission.tsx
@@ -1,8 +1,15 @@
 interface Props {
-    name: string, 
-    status: string,
-    priority: string,
-    description: string
+    setName: (name: string) => void,
+    setStatus: (status: string) => void,
+    setPriority: (priority: string) => void,
+    setDescription: (description: string) => void, 
+}
+
+export default function Add(props: Props) {
+    const [name, setName] = useState('')
+    const [status, setStatus] = useState('')
+    const [priority, setPriority] = useState('')
+    const [description, setDescription] = useState('')  
 }
 export default function AddMission() {
   return (
